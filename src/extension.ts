f82a2c407ba6222105cedc52dd372e0ab7bb52a4
Ritwick Dey <ritwickdey@outlook.com>
2017-12-21 15:05:44 +0530

    removed annoucement

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 import * as vscode from 'vscode';
 import { AppModel } from './appModel';
-import { checkNewAnnouncement } from './announcement/index';
+// import { checkNewAnnouncement } from './announcement/index';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     let appModel = new AppModel();
 
-    checkNewAnnouncement(context.globalState);
+    // checkNewAnnouncement(context.globalState);
 
     let disposablecompileAll =
         vscode.commands.registerCommand('liveSass.command.watchMySass', () => {
