82ebbfa4b2f206aa914dd92b153cf7fa91a157b8
Ritwick Dey <ritwickdey@outlook.com>
2018-06-27 17:41:47 +0530

    small test & ci setup

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,22 +1,33 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+suite('Extension Tests', () => {
+  test('Extension should be present', () => {
+    assert.ok(vscode.extensions.getExtension('ritwickdey.live-sass'));
+  });
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+  test('should activate', function() {
+    this.timeout(1 * 60 * 1000);
+    return vscode.extensions
+      .getExtension('ritwickdey.live-sass')
+      .activate()
+      .then(api => {
+        assert.ok(true);
+      });
+  });
+
+  test('should register all live server commands', function() {
+    return vscode.commands.getCommands(true).then(commands => {
+      const COMMANDS = [
+        'liveSass.command.watchMySass',
+        'liveSass.command.donotWatchMySass',
+        'liveSass.command.oneTimeCompileSass',
+        'liveSass.command.openOutputWindow'
+      ];
+      const foundLiveServerCommands = commands.filter(value => {
+        return COMMANDS.indexOf(value) >= 0 || value.startsWith('liveSass.command.');
+      });
+      assert.equal(foundLiveServerCommands.length, COMMANDS.length);
     });
+  });
 });
