3a2e813085441d6a46f0d6940388cc5c3e5d19c9
TopMarksDevelopment <72769932+TopMarksDevelopment@users.noreply.github.com>
2020-12-29 22:35:58 +0000

    v4.2.0 prep and sass change prep

--- src/SassCompileHelper.ts
+++ src/SassCompileHelper.ts
@@ -1,4 +1,4 @@
-//import { WindowPopout, OutputWindow } from "./VscodeExtensions";
+import { WindowPopout, OutputWindow } from "./VscodeExtensions";
 import { Helper } from "./helper";
 import * as compiler from "sass";
 
@@ -19,53 +19,29 @@ export class SassHelper {
         options: compiler.Options
     ): { result: compiler.Result | null; errorString: string | null } {
         const generateMap = Helper.getConfigSettings<boolean>("generateMap"),
-            //showOutputWindow = Helper.getConfigSettings<boolean>("showOutputWindow"),
+            showOutputWindow = Helper.getConfigSettings<boolean>("showOutputWindow"),
             data: compiler.Options = {};
 
         Object.assign(data, options);
 
         data.file = SassPath;
+        /*data.logger = {
+            warning: (warning: compiler.SassFlag) => {
+                OutputWindow.Show("Warning:", warning.formatted.split("\n"), showOutputWindow);
+                WindowPopout.Warn("Live Sass Compiler\n *Warning:* \n" + warning.formatted);
+            },
+            debug: (debug: compiler.SassFlag) => {
+                OutputWindow.Show("Debug info:", debug.formatted.split("\n"), showOutputWindow);
+            },
+        };*/
 
         if (generateMap) data.sourceMap = mapFileUri;
         else data.omitSourceMapUrl = true;
-
-        /*
-         * TODO: Chase with dart-sass (now just 'sass')
-            See if warn and debug will be accessible properties --> string[] maybe
-            
-        data.functions = {
-            // @error is handled suitably
-            "@warn": function (info) {
-                const lines: string[] = [];
-
-                infoCompiler(lines, info);
-
-                OutputWindow.Show("Warning:", lines, showOutputWindow, true);
-                WindowPopout.Warn("Live Sass Compiler\n *Warning:* \n" + lines.join("\n"));
-
-                return compiler.types.Null;
-            },
-            "@debug": function (info) {
-                const lines: string[] = [];
-
-                infoCompiler(lines, info);
-
-                OutputWindow.Show("Debug info:", lines, showOutputWindow, true);
-
-                return compiler.types.Null;
-            },
-        };//*/
 
         try {
             return { result: compiler.renderSync(data), errorString: null };
         } catch (err) {
             return { result: null, errorString: err.formatted };
         }
-
-        /*function infoCompiler(list: string[], info): void {
-            if (typeof info.getLength === "function")
-                for (let i = 0; i < info.getLength(); i++) infoCompiler(list, info.getValue(i));
-            else list.push(info.getValue().toString() as string);
-        } //*/
     }
 }
