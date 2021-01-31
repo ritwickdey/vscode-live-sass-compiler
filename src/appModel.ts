f2053614b20bdbf23667e275ddcff0119f6dbee7
glen2223 <33450392+glenn2223@users.noreply.github.com>
2021-01-31 02:17:51 +0000

    Various, inc. #22
    - New setting #22: `forceBaseDirectory`
     - You can now specify the subdirectory that searches should start from
    - Fix: Sass excluded check inconsistant
      - `miniMatch` does not consider `glob`'s `options`. I was not aware so there were instances of false positives (brought to light because of tests of #22
    - Fix: `compileCurrentSass` shows wrong message on fail
      - When you run `compileCurrentSass` and it would fail (for whatever reason) it would cause the output to show `Success` rather than `Error` (just the output was wrong, nothing else)
    - Fix: Switches from new eslint setting
      - A new eslint setting caused the extension to break (no release so no public issue)
    - Fix: Status bar inconsistancies during display changes
      - When command bar is changing between visuals it was possible to cause the status and the shown message to be out of sync (due to clicks while setTimeouts are pending)

--- src/appModel.ts
+++ src/appModel.ts
@@ -1,7 +1,5 @@
 "use strict";
 
-import * as autoprefixer from "autoprefixer";
-import * as glob from "glob";
 import * as path from "path";
 import * as vscode from "vscode";
 
@@ -11,6 +9,8 @@ import { ErrorLogger, OutputWindow, WindowPopout } from "./VscodeExtensions";
 import { SassHelper } from "./SassCompileHelper";
 import { StatusBarUi } from "./StatusbarUi";
 
+import autoprefixer from "autoprefixer";
+import glob from "glob";
 import postcss from "postcss";
 
 import BrowserslistError = require("browserslist/error");
@@ -37,26 +37,23 @@ export class AppModel {
     StartWatching(): void {
         const compileOnWatch = Helper.getConfigSettings<boolean>("compileOnWatch");
 
-        if (this.isWatching) {
-            WindowPopout.Inform("Already watching...");
-        } else {
+        if (!this.isWatching) {
             this.isWatching = !this.isWatching;
 
             if (compileOnWatch) {
                 this.compileAllFiles();
-            } else {
-                this.revertUIToWatchingStatusNow();
             }
         }
+
+        this.revertUIToWatchingStatusNow();
     }
 
     StopWatching(): void {
         if (this.isWatching) {
             this.isWatching = !this.isWatching;
-            this.revertUIToWatchingStatusNow();
-        } else {
-            WindowPopout.Inform("Not watching...");
         }
+
+        this.revertUIToWatchingStatusNow();
     }
 
     openOutputWindow(): void {
@@ -151,16 +148,21 @@ export class AppModel {
             );
 
             const formats = Helper.getConfigSettings<IFormat[]>("formats");
-            await Promise.all(
+            const result = await Promise.all(
                 formats.map(async (format) => {
                     // Each format
                     const options = this.getCssStyle(format.format),
                         cssMapUri = await this.generateCssAndMapUri(sassPath, format);
-                    await this.GenerateCssAndMap(sassPath, cssMapUri.css, cssMapUri.map, options);
+                    return await this.GenerateCssAndMap(
+                        sassPath,
+                        cssMapUri.css,
+                        cssMapUri.map,
+                        options
+                    );
                 })
             );
 
-            StatusBarUi.compilationSuccess(this.isWatching);
+            if (result.indexOf(false) < 0) StatusBarUi.compilationSuccess(this.isWatching);
         } catch (err) {
             const sassPath = vscode.window.activeTextEditor
                 ? vscode.window.activeTextEditor.document.fileName
@@ -461,7 +463,6 @@ export class AppModel {
         const showOutputWindow = Helper.getConfigSettings<boolean>("showOutputWindow"),
             generateMap = Helper.getConfigSettings<boolean>("generateMap"),
             prefixer = postcss(
-                // @ts-ignore
                 autoprefixer({
                     overrideBrowserslist: browsers,
                 })
@@ -534,40 +535,57 @@ export class AppModel {
 
     private async isSassFileExcluded(sassPath: string): Promise<boolean> {
         const excludeItems = Helper.getConfigSettings<string[]>("excludeList"),
-            includeItems = Helper.getConfigSettings<string[] | null>("includeItems");
-        let fileList: string[];
+            includeItems = Helper.getConfigSettings<string[] | null>("includeItems"),
+            forceBaseDirectory = Helper.getConfigSettings<string | null>("forceBaseDirectory");
+        let fileList = ["**/*.s[a|c]ss"];
 
         if (includeItems && includeItems.length) {
             fileList = includeItems.concat("**/_*.s[a|c]ss");
-        } else fileList = excludeItems;
+        }
 
         const fileFound = (
             await Promise.all(
-                vscode.workspace.workspaceFolders.map((folder) => {
-                    return glob(
-                        `{${fileList.join(",")}}`,
-                        {
-                            ignore: includeItems && includeItems.length ? excludeItems : null,
-                            mark: true,
-                            cwd: folder.uri.fsPath,
-                        },
-                        (err) => {
-                            if (err) {
-                                OutputWindow.Show(
-                                    "Error whilst searching for files",
-                                    [`Workspace folder: ${folder.name}`, err.message, err.stack],
-                                    true
+                vscode.workspace.workspaceFolders.map(async (folder) => {
+                    let basePath = folder.uri.fsPath;
+
+                    if (forceBaseDirectory) basePath = path.resolve(basePath, forceBaseDirectory);
+
+                    return await new Promise((resolve) => {
+                        glob(
+                            `{${fileList.join(",")},}`,
+                            {
+                                ignore: excludeItems,
+                                mark: true,
+                                cwd: basePath,
+                            },
+                            (err, files) => {
+                                if (err) {
+                                    OutputWindow.Show(
+                                        "Error whilst searching for files",
+                                        [
+                                            `Workspace folder: ${folder.name}`,
+                                            err.message,
+                                            err.stack,
+                                        ],
+                                        true
+                                    );
+
+                                    return resolve(false);
+                                }
+
+                                resolve(
+                                    files.indexOf(
+                                        path.relative(basePath, sassPath).split(path.sep).join("/")
+                                    ) >= 0
                                 );
                             }
-                        }
-                    ).minimatch.match(sassPath);
+                        );
+                    });
                 })
             )
         ).includes(true);
 
-        if (includeItems && includeItems.length) return fileFound === false;
-
-        return fileFound;
+        return fileFound == false;
     }
 
     private async getSassFiles(
@@ -578,7 +596,8 @@ export class AppModel {
         const excludedList = isDebugging
                 ? ["**/node_modules/**", ".vscode/**"]
                 : Helper.getConfigSettings<string[]>("excludeList"),
-            includeItems = Helper.getConfigSettings<string[] | null>("includeItems");
+            includeItems = Helper.getConfigSettings<string[] | null>("includeItems"),
+            forceBaseDirectory = Helper.getConfigSettings<string | null>("forceBaseDirectory");
 
         if (!isQueryPatternFixed && includeItems && includeItems.length) {
             if (includeItems.length === 1) {
@@ -592,6 +611,10 @@ export class AppModel {
 
         await Promise.all(
             vscode.workspace.workspaceFolders.map(async (folder) => {
+                let basePath = folder.uri.fsPath;
+
+                if (forceBaseDirectory) basePath = path.resolve(basePath, forceBaseDirectory);
+
                 (
                     await new Promise((resolve: (value: string[]) => void) => {
                         glob(
@@ -599,7 +622,7 @@ export class AppModel {
                             {
                                 ignore: excludedList,
                                 mark: true,
-                                cwd: folder.uri.fsPath,
+                                cwd: basePath,
                             },
                             (err, files: string[]) => {
                                 if (err) {
@@ -619,7 +642,7 @@ export class AppModel {
                                     .filter((file) =>
                                         this.isSassFile(file, isDebugging || isQueryPatternFixed)
                                     )
-                                    .map((file) => path.join(folder.uri.fsPath, file));
+                                    .map((file) => path.join(basePath, file));
                                 return resolve(filePaths || []);
                             }
                         );
